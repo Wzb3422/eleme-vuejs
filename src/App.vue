61aa2758937102eb6084f73a566ced0308910757
wzb <wzb3422@gmail.com>
2019-03-17 20:31:21 +0800

    :construction: add axios

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
-    <Header />
+    <Header :seller="this.seller" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
     Header
+  },
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  mounted () {
+    axios.get('/api/seller')
+      .then((res) => {
+        this.seller = res.data
+        console.log(this.seller)
+      })
+    console.log(this.seller)
   }
 }
 </script>
